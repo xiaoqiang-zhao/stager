1a78adce784445edde8834ca99abd31f1ea83988
longze <longze_xq@163.com>
2018-05-25 11:29:32 +0800

    样式和功能修改完成

--- client/pages/appointment/index.js
+++ client/pages/appointment/index.js
@@ -6,14 +6,6 @@ Page({
     locationName: '百度科技园附近'
   },
   onLoad() {
-    // wx.chooseLocation({
-    //     success: res => {
-    //         this.locationName = res.name;
-    //     },
-    //     fail: res => {
-    //         this.locationName = '未授权获取当前位置';
-    //     }
-    // });
     wx.setNavigationBarTitle({
       title: '预约'
     });
--- client/pages/to-be-stager/index.js
+++ client/pages/to-be-stager/index.js
@@ -3,7 +3,8 @@
 Page({
   data: {
     userInfo: {},
-    locationName: '百度科技园附近'
+    locationName: '百度科技园附近',
+    count: 0
   },
   onLoad() {
     // wx.chooseLocation({
@@ -18,6 +19,15 @@ Page({
       title: '成为过来人'
     });
   },
+
+  // 文本域输入事件
+  onInput(event) {
+    this.setData({
+      count: event.detail.cursor
+    });
+  },
+
+  // 提交
   submit() {
     wx.showToast({
       title: '提价成功',
